86a89312b1135138f4fedb8fa2ef5b6a98224c29
JAYANT SINGH <97082996+jayantsB123@users.noreply.github.com>
2024-11-07 09:47:13 +0530

    Update Assign6-DAA.cpp

--- DAA-main/Assign6-DAA.cpp
+++ DAA-main/Assign6-DAA.cpp
@@ -29,7 +29,7 @@ int partition(vector<int>& arr, int low, int high) {
 void quickSort(vector<int>& arr, int low, int high) {
 
 	if (low < high) {
-		int pi = partition1(arr, low, high);
+		int pi = partition(arr, low, high);
 
 		quickSort(arr, low, pi - 1);
 		quickSort(arr, pi + 1, high);
